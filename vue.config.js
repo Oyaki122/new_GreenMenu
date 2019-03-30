e4cfbe87c866ee22778e60c6d375ea7692c14a4c
Oyaki <oc.yaki122@gmail.com>
2019-03-30 22:00:47 +0900

    fix vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  outputDir: '/mnt/d/Document/html/Green_new/green-new/docs'
+  outputDir: '/mnt/d/Document/html/Green_new/green-new/docs',
+  build: {
+    assetsPublicPath: ''
+  }
 }
