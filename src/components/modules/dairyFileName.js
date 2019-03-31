6fa4e7bff03039d8bd1ec34c459c7850ab05fc1c
Oyaki <oc.yaki122@gmail.com>
2019-03-31 22:38:41 +0900

    ファイル指定を、その週の月曜日を基準に下

--- src/components/modules/dairyFileName.js
+++ src/components/modules/dairyFileName.js
@@ -1,5 +1,6 @@
 export default function dairyFileName (date) {
-  const today = date
+  const today = new Date(date)
+  if (today.getDay() !== 1) today.setDate(today.getDate() - today.getDay() + 1)
   const howWeek = Math.floor((today.getDate() - today.getDay() + 12) / 7)
   const fileName =
     today.getFullYear() + '_' + (today.getMonth() + 1) + '_' + howWeek + '.csv'
